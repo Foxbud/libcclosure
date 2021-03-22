176c39e7aeb32eab6b7aa6a97978d36f38c9a690
Foxbud <breadboardfox@gmail.com>
2021-03-22 11:58:08 -0400

    Added license.

--- src/cclosure.c
+++ src/cclosure.c
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Garrett Fairburn <breadboardfox@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #define _GNU_SOURCE 1
 
 #include <stdint.h>
@@ -54,15 +69,6 @@ typedef struct MemBank {
     pthread_rwlock_t lock;
 #endif
 } MemBank;
-
-/* ----- PUBLIC CONSTANTS ----- */
-
-CCLOSURE_EXPORT const CClosureThreadType CCLOSURE_THREAD_TYPE =
-#ifdef THREAD_PTHREADS
-    CCLOSURE_THREAD_PTHREADS;
-#else
-    CCLOSURE_THREAD_NONE;
-#endif
 
 /* ----- PRIVATE CONSTANTS ----- */
 
@@ -126,6 +132,15 @@ static const uint8_t THUNK_NORM_RET[THUNK_SIZE] = {
 static const uint8_t THUNK_AGG_RET[THUNK_SIZE] = {
     0xE8, 0x0C, 0x00, 0x00, 0x00, 0x5A, 0x59, 0x52, 0xFF, 0x70, 0x14,
     0x51, 0xFF, 0x50, 0x10, 0x59, 0xC3, 0x8B, 0x04, 0x24, 0xC3};
+#endif
+
+/* ----- PUBLIC CONSTANTS ----- */
+
+CCLOSURE_EXPORT const CClosureThreadType CCLOSURE_THREAD_TYPE =
+#ifdef THREAD_PTHREADS
+    CCLOSURE_THREAD_PTHREADS;
+#else
+    CCLOSURE_THREAD_NONE;
 #endif
 
 /* ----- PRIVATE GLOBALS ----- */
