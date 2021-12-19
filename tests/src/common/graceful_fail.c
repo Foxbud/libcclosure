2139d2f79803dde205c8f128170b77ad64cde1f1
Foxbud <breadboardfox@gmail.com>
2021-12-18 20:45:22 -0500

    Added test case for graceful fail.

---
+++ tests/src/common/graceful_fail.c
@@ -0,0 +1,26 @@
+/* Verify that invoking an uninitialized macro causes an illegal instruction
+ * fault. */
+
+#include <signal.h>
+
+#include "test_prelude.h"
+
+void Callback0(void) { return; }
+
+void SigIllHandler(int signum) {
+    AssertIntEqual(signum, SIGILL);
+
+    Pass();
+}
+
+TestCase {
+    void (*closure)(void) = CClosureNew(Callback0, NULL, false);
+    AssertBoolEqual(CClosureCheck(closure), true);
+    CClosureFree(closure);
+    AssertBoolEqual(CClosureCheck(closure), false);
+
+    signal(SIGILL, SigIllHandler);
+    closure();
+
+    Fail("Execution did not abort!");
+}
