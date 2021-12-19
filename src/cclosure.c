2396a7677354e0574aa5f0ed5b6ebc9f62ba3b50
Foxbud <breadboardfox@gmail.com>
2021-12-18 21:41:38 -0500

    Closures can now free themselves.

--- src/cclosure.c
+++ src/cclosure.c
@@ -34,51 +34,52 @@
 
 #ifdef __LP64__
 #define IsAggRet(clos) (false)
+
+#define THUNK_ENTRY_SIZE 26
+#define THUNK_EXIT_SIZE 8
 #else
-#define IsAggRet(clos) (clos->bin[0] == 0x5a)
+#define IsAggRet(clos) (clos->entry.bin[0] == 0x5a)
+
+#define THUNK_ENTRY_SIZE 14
+#define THUNK_EXIT_SIZE 6
 #endif
 
 /* ----- PRIVATE TYPES ----- */
 
-/* DEBUG */
-
-typedef union Closure {
+typedef struct __attribute__((packed)) Closure {
 #ifdef __LP64__
-    uint8_t bin[34];
     union {
-        struct __attribute__((packed)) {
-            uint8_t pad0[6];
-            void *env;
-            uint8_t pad1[4];
-            void *fcn;
-            uint8_t pad2[8];
-        } norm;
-        struct __attribute__((packed)) {
-            uint8_t pad0[6];
-            void *env;
-            uint8_t pad1[4];
-            void *fcn;
-            uint8_t pad2[8];
-        } agg;
-    } tmpl;
+        uint8_t bin[THUNK_ENTRY_SIZE];
+        union {
+            struct __attribute__((packed)) {
+                uint8_t pad0[6];
+                void *env;
+                uint8_t pad1[4];
+                void *fcn;
+            } norm, agg;
+        } tmpl;
+    } entry;
+    const uint8_t exit[THUNK_EXIT_SIZE];
 #else
-    uint8_t bin[20];
     union {
-        struct __attribute__((packed)) {
-            uint8_t pad0[1];
-            void *env;
-            uint8_t pad1[1];
-            void *fcn;
-            uint8_t pad2[6];
-        } norm;
-        struct __attribute__((packed)) {
-            uint8_t pad0[4];
-            void *env;
-            uint8_t pad1[2];
-            void *fcn;
-            uint8_t pad2[6];
-        } agg;
-    } tmpl;
+        uint8_t bin[THUNK_ENTRY_SIZE];
+        union {
+            struct __attribute__((packed)) {
+                uint8_t pad0[1];
+                void *env;
+                uint8_t pad1[1];
+                void *fcn;
+                uint8_t pad2[4];
+            } norm;
+            struct __attribute__((packed)) {
+                uint8_t pad0[4];
+                void *env;
+                uint8_t pad1[2];
+                void *fcn;
+            } agg;
+        } tmpl;
+    } entry;
+    const uint8_t exit[THUNK_EXIT_SIZE];
 #endif
 } Closure;
 
@@ -114,61 +115,92 @@ typedef struct MemBank {
  * %define tmpl_env strict QWORD 0
  * %define tmpl_fcn strict QWORD 0
  *
- * closure_x86_64:
+ * thunk_entry_norm_x86_64:
  * 		sub rsp, 8 * 2
  * 		mov r11, tmpl_env
  * 		push r11
  * 		mov r11, tmpl_fcn
+ */
+static const uint8_t THUNK_ENTRY_NORM[THUNK_ENTRY_SIZE] = {
+    0x48, 0x83, 0xec, 0x10, 0x49, 0xbb, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x41, 0x53, 0x49, 0xbb,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+static const uint8_t *THUNK_ENTRY_AGG = THUNK_ENTRY_NORM;
+
+/* BITS 64
+ *
+ * thunk_entry_uninit_x86_64:
+ * 		times 24 nop
+ * 		ud2
+ */
+static const uint8_t THUNK_ENTRY_UNINIT[THUNK_ENTRY_SIZE] = {
+    0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90,
+    0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90,
+    0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x0f, 0x0b};
+
+/* BITS 64
+ *
+ * thunk_exit_x86_64:
  * 		call r11
  * 		add rsp, 8 * 3
  * 		ret
  */
-static const uint8_t THUNK_NORM_RET[] = {
-    0x48, 0x83, 0xec, 0x10, 0x49, 0xbb, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x41, 0x53, 0x49, 0xbb, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x41, 0xff, 0xd3, 0x48, 0x83, 0xc4, 0x18, 0xc3};
-static const size_t THUNK_NORM_RET_SIZE = 34;
-
-static const uint8_t *THUNK_AGG_RET = THUNK_NORM_RET;
-static const size_t THUNK_AGG_RET_SIZE = THUNK_NORM_RET_SIZE;
+static const uint8_t THUNK_EXIT[THUNK_EXIT_SIZE] = {0x41, 0xff, 0xd3, 0x48,
+                                                    0x83, 0xc4, 0x18, 0xc3};
 #else
 /* BITS 32
  *
  * %define tmpl_env strict DWORD 0
  * %define tmpl_fcn strict DWORD 0
  *
- * closure_x86_norm:
- * 		push tmpl_env
+ * thunk_entry_norm_x86:
+ *      push tmpl_env
  * 		mov ecx, tmpl_fcn
- * 		call ecx
- * 		add esp, 4
- * 		ret
+ * 		jmp thunk_entry_uninit_x86
+ * 		ud2;
+ * thunk_entry_uninit_x86:
  */
-static const uint8_t THUNK_NORM_RET[] = {0x68, 0x00, 0x00, 0x00, 0x00, 0xb9,
-                                         0x00, 0x00, 0x00, 0x00, 0xff, 0xd1,
-                                         0x83, 0xc4, 0x04, 0xc3};
-static const size_t THUNK_NORM_RET_SIZE = 16;
+static const uint8_t THUNK_ENTRY_NORM[THUNK_ENTRY_SIZE] = {
+    0x68, 0x00, 0x00, 0x00, 0x00, 0xb9, 0x00,
+    0x00, 0x00, 0x00, 0xeb, 0x02, 0x0f, 0x0b};
 
 /* BITS 32
  *
  * %define tmpl_env strict DWORD 0
  * %define tmpl_fcn strict DWORD 0
  *
- * closure_x86_agg:
+ * thunk_entry_agg_x86:
  * 		pop edx
  * 		pop ecx
  * 		push edx
  * 		push tmpl_env
  * 		push ecx
  * 		mov ecx, tmpl_fcn
- * 		call ecx
- * 		add esp, 4
- * 		ret
  */
-static const uint8_t THUNK_AGG_RET[] = {
-    0x5a, 0x59, 0x52, 0x68, 0x00, 0x00, 0x00, 0x00, 0x51, 0xb9,
-    0x00, 0x00, 0x00, 0x00, 0xff, 0xd1, 0x83, 0xc4, 0x04, 0xc3};
-static const size_t THUNK_AGG_RET_SIZE = 20;
+static const uint8_t THUNK_ENTRY_AGG[THUNK_ENTRY_SIZE] = {
+    0x5a, 0x59, 0x52, 0x68, 0x00, 0x00, 0x00,
+    0x00, 0x51, 0xb9, 0x00, 0x00, 0x00, 0x00};
+
+/* BITS 32
+ *
+ * thunk_entry_uninit_x86:
+ * 		times 12 nop
+ * 		ud2
+ */
+static const uint8_t THUNK_ENTRY_UNINIT[THUNK_ENTRY_SIZE] = {
+    0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90,
+    0x90, 0x90, 0x90, 0x90, 0x90, 0x0f, 0x0b};
+
+/* BITS 32
+ *
+ * thunk_exit_x86:
+ *  		call ecx
+ *  		add esp, 4
+ *  		ret
+ */
+static const uint8_t THUNK_EXIT[THUNK_EXIT_SIZE] = {0xff, 0xd1, 0x83,
+                                                    0xc4, 0x04, 0xc3};
 #endif
 
 /* ----- PUBLIC CONSTANTS ----- */
@@ -208,10 +240,14 @@ static void MemBlockInit(MemBlock *block, size_t blockIdx) {
 
     for (size_t idx = 0; idx < cap - 1; idx++) {
         MemSlot *slot = block->slots + idx;
+        memcpy(slot->clos.entry.bin, THUNK_ENTRY_UNINIT, THUNK_ENTRY_SIZE);
+        memcpy((void *)slot->clos.exit, THUNK_EXIT, THUNK_EXIT_SIZE);
         slot->nextFree = slot + 1;
         *(size_t *)&slot->blockIdx = blockIdx;
     }
     MemSlot *last = block->slots + cap - 1;
+    memcpy(last->clos.entry.bin, THUNK_ENTRY_UNINIT, THUNK_ENTRY_SIZE);
+    memcpy((void *)last->clos.exit, THUNK_EXIT, THUNK_EXIT_SIZE);
     last->nextFree = NULL;
     *(size_t *)&last->blockIdx = blockIdx;
 
@@ -302,16 +338,16 @@ CCLOSURE_EXPORT void *CClosureNew(void *fcn, void *env, bool aggRet) {
     pthread_setcancelstate(origCancelState, &origCancelState);
 #endif
 
-    /* Initialize closure. */
+    /* Initialize closure entry. */
     Closure *clos = (Closure *)slot;
     if (aggRet) {
-        memcpy(clos->bin, THUNK_AGG_RET, THUNK_AGG_RET_SIZE);
-        clos->tmpl.agg.fcn = fcn;
-        clos->tmpl.agg.env = env;
+        memcpy(clos->entry.bin, THUNK_ENTRY_AGG, THUNK_ENTRY_SIZE);
+        clos->entry.tmpl.agg.fcn = fcn;
+        clos->entry.tmpl.agg.env = env;
     } else {
-        memcpy(clos->bin, THUNK_NORM_RET, THUNK_NORM_RET_SIZE);
-        clos->tmpl.norm.fcn = fcn;
-        clos->tmpl.norm.env = env;
+        memcpy(clos->entry.bin, THUNK_ENTRY_NORM, THUNK_ENTRY_SIZE);
+        clos->entry.tmpl.norm.fcn = fcn;
+        clos->entry.tmpl.norm.env = env;
     }
 
     return clos;
@@ -319,9 +355,10 @@ CCLOSURE_EXPORT void *CClosureNew(void *fcn, void *env, bool aggRet) {
 
 CCLOSURE_EXPORT void *CClosureFree(void *clos) {
 #define clos ((Closure *)clos)
-    /* Deinitialize closure. */
-    void *env = (IsAggRet(clos)) ? clos->tmpl.agg.env : clos->tmpl.norm.env;
-    *clos = (Closure){0};
+    /* Deinitialize closure entry. */
+    void *env =
+        (IsAggRet(clos)) ? clos->entry.tmpl.agg.env : clos->entry.tmpl.norm.env;
+    memcpy(clos->entry.bin, THUNK_ENTRY_UNINIT, THUNK_ENTRY_SIZE);
 
     /* Release free slot. */
     MemSlot *slot = (MemSlot *)clos;
@@ -361,7 +398,7 @@ CCLOSURE_EXPORT bool CClosureCheck(void *clos) {
             pthread_rwlock_rdlock(&block->lock);
             pthread_cleanup_push(UnlockRwLock, &block->lock);
 #endif
-            result = ((Closure *)clos)->bin[0] != 0;
+            result = ((Closure *)clos)->entry.bin[0] != 0x90;
 #ifdef THREAD_PTHREADS
             pthread_cleanup_pop(true);
 #endif
@@ -378,12 +415,14 @@ CCLOSURE_EXPORT bool CClosureCheck(void *clos) {
 
 CCLOSURE_EXPORT void *CClosureGetFcn(void *clos) {
 #define clos ((Closure *)clos)
-    return (IsAggRet(clos)) ? clos->tmpl.agg.fcn : clos->tmpl.norm.fcn;
+    return (IsAggRet(clos)) ? clos->entry.tmpl.agg.fcn
+                            : clos->entry.tmpl.norm.fcn;
 #undef clos
 }
 
 CCLOSURE_EXPORT void *CClosureGetEnv(void *clos) {
 #define clos ((Closure *)clos)
-    return (IsAggRet(clos)) ? clos->tmpl.agg.env : clos->tmpl.norm.env;
+    return (IsAggRet(clos)) ? clos->entry.tmpl.agg.env
+                            : clos->entry.tmpl.norm.env;
 #undef clos
 }
